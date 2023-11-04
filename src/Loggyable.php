d383a504e5d71c543dd38249963af855ce8f41e3
Farzin Bidokhti <Farzin.bidokhti@Gmail.com>
2023-11-04 21:56:45 +0330

    Ip address, device and browser agent added

--- src/Loggyable.php
+++ src/Loggyable.php
@@ -7,36 +7,69 @@ use FarzinBidokhti\Loggy\Loggy;
 trait Loggyable
 {
     /**
-   * create a log record
-   *
-   * @param integer  $userId The log message to write
-   * @param string   $ipAddress The log level e.g. INFO, WARNING, ERROR
-   * @param string   $action The log level e.g. INFO, WARNING, ERROR
-   * @param string   $actionDetail The log level e.g. INFO, WARNING, ERROR
-   * @param boolean  $status The log level e.g. INFO, WARNING, ERROR
-   * @param string   $device The log level e.g. INFO, WARNING, ERROR
-   * @param string   $browserAgent The log level e.g. INFO, WARNING, ERROR
-   * @return integer return log id if created successfully
-   */
+     * create a log record
+     *
+     * @param integer  $userId The log message to write
+     * @param string   $action The log level e.g. INFO, WARNING, ERROR
+     * @param string   $actionDetail The log level e.g. INFO, WARNING, ERROR
+     * @param boolean  $status The log level e.g. INFO, WARNING, ERROR
+     * @return integer return log id if created successfully
+     */
     public static function log(
         $userId,
-        $ipAddress    = null,
         $action       = null,
         $actionDetail = null,
-        $status       = null,
-        $device       = null,
-        $browserAgent = null
+        $status       = null
     ) {
         $loggy                = new Loggy();
         $loggy->user_id       = $userId;
-        $loggy->ip_address    = $ipAddress;
+        $loggy->ip_address    = self::getIp();
         $loggy->action        = $action;
         $loggy->action_detail = $actionDetail;
         $loggy->status        = $status;
-        $loggy->device        = $device;
-        $loggy->browser_agent = $browserAgent;
+        $loggy->device        = self::getDevice();
+        $loggy->browser_agent = self::getBrowserAgent();
         $loggy->save();
 
-        return $loggy->id;
+        return Loggy::findOrFail($loggy->id);
+    }
+
+    private static function getIp()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+
+        if (!empty($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+
+        if (filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP)) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+
+        return 'Unable to get valid user IP';
+    }
+
+    private static function getBrowserAgent()
+    {
+        return $_SERVER['HTTP_USER_AGENT'];
+    }
+
+    private static function getDevice()
+    {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false) {
+            return 'mobile';
+        }
+
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'Tablet') !== false) {
+            return 'tablet';
+        }
+
+        return 'desktop';
     }
 }
